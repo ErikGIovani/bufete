7b15c1916293c6dd0cd0e48f673685eca5311f4b
ErikGIovani <46170949+ErikGIovani@users.noreply.github.com>
2021-05-18 01:38:44 -0500

    Add files via upload

---
+++ assets/scripts/sweet-alert.js
@@ -0,0 +1,55 @@
+function executeExample() {
+    Swal.fire({
+        title: '<i style="margin: 1vw; font-size: 5vw; color: black" class="fas fa-scroll"></i> <br> <h2 style="color: black">Derecho Laboral</h2>',
+        html:'<p style="color: #494949">Tratamos indemnizaciones por despidos, accidentes de trabajo, liquidaciones finales, sindicales, contratos laborales y asesorías en general para empleadores y trabajadores</p>',
+        confirmButtonText:'<button style="font-size: 1.5vw; padding: 1vw 2vw; border-radius: 2vw; border: 0.2vw solid black; background: transparent">Cerrar</button>',
+      });
+  }
+
+    function executeExample2() {
+        Swal.fire({
+            title: '<i style="margin: 1vw; font-size: 5vw; color: black" class="fas fa-scroll"></i> <br> <h2 style="color: black">Civil Y Comercial</h2>',
+            html:'<p style="color: #494949">Realizamos demandas por daños y perjuicios, contratos de todo tipo, asesoramos en cuestiones de sociedades civiles y comerciales</p>',
+            confirmButtonText:'<button style="font-size: 1.5vw; padding: 1vw 2vw; border-radius: 2vw; border: 0.2vw solid black; background: transparent">Cerrar</button>',
+        });
+    }
+
+    function executeExample3() {
+        Swal.fire({
+            title: '<i style="margin: 1vw; font-size: 5vw; color: black" class="fas fa-scroll"></i> <br> <h2 style="color: black">Derecho Penal</h2>',
+            html:'<p style="color: #494949">Realizamos excarcelaciones expeditas, representamos durante toda la etapa de instrucción y el juicio oral, apelaciones de condenas, restitución de objetos y asesoramos para la realización de denuncias</p>',
+            confirmButtonText:'<button style="font-size: 1.5vw; padding: 1vw 2vw; border-radius: 2vw; border: 0.2vw solid black; background: transparent">Cerrar</button>',
+          });
+      }
+
+      function executeExample4() {
+        Swal.fire({
+            title: '<i style="margin: 1vw; font-size: 5vw; color: black" class="fas fa-scroll"></i> <br> <h2 style="color: black">Mediación</h2>',
+            html:'<p style="color: #494949">Realizamos mediaciones en general, una forma de solucionar sus conflictos de una manera rápida y justa, ya que hoy en día hay cuestiones que se pueden resolver de manera expedita fuera del ámbito de los juzgados, ahorrando tiempo y dinero</p>',
+            confirmButtonText:'<button style="font-size: 1.5vw; padding: 1vw 2vw; border-radius: 2vw; border: 0.2vw solid black; background: transparent">Cerrar</button>',
+          });
+      }
+
+      function executeExample5() {
+        Swal.fire({
+            title: '<i style="margin: 1vw; font-size: 5vw; color: black" class="fas fa-scroll"></i> <br> <h2 style="color: black">Juicios Ejecutivos</h2>',
+            html:'<p style="color: #494949">Nos encargamos de que pueda hacer valer sus títulos ejecutivos, con rápidas demanadas ejecutivas, en el que podrá cobrar el dinero que le corresponde</p>',
+            confirmButtonText:'<button style="font-size: 1.5vw; padding: 1vw 2vw; border-radius: 2vw; border: 0.2vw solid black; background: transparent">Cerrar</button>',
+          });
+      }
+
+      function executeExample6() {
+        Swal.fire({
+            title: '<i style="margin: 1vw; font-size: 5vw; color: black" class="fas fa-scroll"></i> <br> <h2 style="color: black">Derecho De Familia Y Sucesiones</h2>',
+            html:'<p style="color: #494949">Todo tipo de cuestiones de derecho de familia, guarda de menores, régimen de visitas, patria potestad, filiaciones, alimentos, divorcios, acuerdos pre-nupciales, cuestión referida a los bienes, sucesiones con testamento y abintestato</p>',
+            confirmButtonText:'<button style="font-size: 1.5vw; padding: 1vw 2vw; border-radius: 2vw; border: 0.2vw solid black; background: transparent">Cerrar</button>',
+          });
+      }
+
+      function executeExample7() {
+        Swal.fire({
+            title: '<i style="margin: 1vw; font-size: 5vw; color: black" class="fas fa-scroll"></i> <br> <h2 style="color: black">Recursos Administrativos Y Cuestiones Con La AFIP</h2>',
+            html:'<p style="color: #494949">Defiéndase en litigios contenciosos administrativos, o reclame lo que le corresponde por derecho al estado. Trabajamos con cuestiones contra la AFIP</p>',
+            confirmButtonText:'<button style="font-size: 1.5vw; padding: 1vw 2vw; border-radius: 2vw; border: 0.2vw solid black; background: transparent">Cerrar</button>',
+          });
+      }
